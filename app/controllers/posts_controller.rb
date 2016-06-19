c6bc9f71b23c990782a5debe30c56d20201ccc88
CorainChicago <chaysmagan@gmail.com>
2016-06-19 18:14:00 -0500

    setup controller for Post with index and a destory method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,14 @@
 class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all
+  end
+
+  def destroy
+    Post.find(params[:id]).destroy
+    respond_to do |format|
+      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
 end
